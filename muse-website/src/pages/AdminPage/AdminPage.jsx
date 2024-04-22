0d31716d18f24422f0234d2f9d0511e787e6c7b9
mira-programs <mirahussein06@gmail.com>
2024-04-23 01:16:12 +0300

    adding warnings and reports admin pages (skeleton)

--- muse-website/src/pages/AdminPage/AdminPage.jsx
+++ muse-website/src/pages/AdminPage/AdminPage.jsx
@@ -13,12 +13,12 @@ const AdminPage = () => {
         <h2 className="title">Admin Features</h2>
         <div className="grid">
           <article className="item">
-            <h3 className="item-title">Reports</h3>
-            <p className="item-description">Different reports that users have submitted.</p>
+            <h3 className="item-title">Home</h3>
+            <p className="item-description">The posts are displayed and can be deleted.</p>
           </article>
           <article className="item">
-            <h3 className="item-title">Post Management</h3>
-            <p className="item-description">View posts and delete them if needed.</p>
+            <h3 className="item-title">Reports</h3>
+            <p className="item-description">All reports submitted by users.</p>
           </article>
           <article className="item">
             <h3 className="item-title">Warnings</h3>
---
+++ muse-website/src/pages/AdminReports/AdminReports.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const AdminReports = () => {
+  return (
+    <main>
+      <header className="hero-container">
+        <h2 className="title">Report Page</h2>
+      </header>
+
+      <section className="section section-explore">
+        <div className="solid-color-background"></div>
+        <h2 className="title">Reports page</h2>
+        <div className="grid">
+          <article className="item">
+            <h3 className="item-title">Reports</h3>
+            <p className="item-description">List the reports.</p>
+          </article>
+        </div>
+      </section>
+
+    </main>
+  );
+};
+
+export default AdminReports;
