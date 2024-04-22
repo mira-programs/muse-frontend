722ab4ae79e716417f44865cdba46738f4ed26dc
mira-programs <mirahussein06@gmail.com>
2024-04-23 00:29:47 +0300

    creating admin header files

---
+++ muse-website/src/components/AdminHeader/AdminHeader.jsx
@@ -0,0 +1,28 @@
+import './AdminHeader.css'
+import { Link } from 'react-router-dom'
+
+// import react icons................
+import { FaBars } from "react-icons/fa";
+import { AiOutlineClose } from "react-icons/ai";
+
+export default function Header() {
+  return (
+    <nav id="header">
+        <div className="container nav-container">
+            <Link className="nav-logo" to={'/home'}>MUSE</Link>
+
+            <ul className="nav-menu">
+                <li><Link to={'/profile/id'}>Username</Link></li>
+                <li><Link to={'/create'}>Create Post</Link></li>
+                {/* <li><Link to={'/muser'}>Musers</Link></li> */}
+                {/* <li><Link to={'/logout'}>Logout</Link></li> */}
+            </ul>
+
+            <button className="nav-toggle-button">
+            <FaBars />
+            <AiOutlineClose />
+            </button>
+        </div>
+    </nav>
+  )
+}
