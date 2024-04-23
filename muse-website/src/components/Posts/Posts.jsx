1101a8b3254f8c7ce646b2221810073f72c8432c
Dala Ibrahim <dalaibrahim10@gmail.com>
2024-04-23 20:57:32 +0300

    same changes

--- muse-website/src/components/Posts/Posts.jsx
+++ muse-website/src/components/Posts/Posts.jsx
@@ -1,33 +1,31 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios'; 
-import PostItem from '../PostItem/PostItem'; 
+import axios from 'axios';
+import PostItem from '../PostItem/PostItem';
+
 
 const Posts = () => {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
+
   useEffect(() => {
     fetchPosts();
   }, []);
-  
+ 
     const fetchPosts = async () => {
       try {
         const response = await axios.get('http://localhost:8080/posts');
         let data = response.data;
 
+
         if (!Array.isArray(data)) {
-          data = [data]; 
+          data = [data];
         }
+
 
         if (data.length > 0) {
           setPosts(data);
-=======
-        const response = await axios.get('/api/posts');
-        // Check if the response contains data and it's an array
-        if (response.data && Array.isArray(response.data)) {
-          setPosts(response.data);
->>>>>>> 36f065570696933742b5a0deb36d3d1b9b3dddac
         } else {
           setError('No posts found');
         }
@@ -39,13 +37,17 @@ const Posts = () => {
     };
 
 
+
+
   if (loading) {
     return <div>Loading...</div>;
   }
 
+
   if (error) {
     return <div>Error: {error}</div>;
   }
+
 
   return (
     <section className="posts">
@@ -65,5 +67,6 @@ const Posts = () => {
     </section>
   );
 };
+
 
 export default Posts;
