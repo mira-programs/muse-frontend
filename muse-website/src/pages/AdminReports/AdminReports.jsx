25acd7c309867147b247872b6959253a2031f981
mira-programs <mirahussein06@gmail.com>
2024-04-23 22:53:24 +0300

    displaying reports on reports page

--- muse-website/src/pages/AdminReports/AdminReports.jsx
+++ muse-website/src/pages/AdminReports/AdminReports.jsx
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const AdminReports = () => {
+  const [reports, setReports] = useState([]);
+
+  useEffect(() => {
+    fetchReports();
+  }, []);
+
+  const fetchReports = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/report'); // Replace with your backend endpoint
+      const data = await response.json();
+      setReports(data);
+    } catch (error) {
+      console.error('Error fetching reports:', error);
+    }
+  };
+
   return (
     <main>
       <header className="hero-container">
@@ -11,13 +27,16 @@ const AdminReports = () => {
         <div className="solid-color-background"></div>
         <h2 className="title">Reports page</h2>
         <div className="grid">
-          <article className="item">
-            <h3 className="item-title">Reports</h3>
-            <p className="item-description">List the reports.</p>
-          </article>
+          {reports.map(report => (
+            <article key={report._id} className="item">
+              <h3 className="item-title">Report ID: {report._id}</h3>
+              <p className="item-description">Reported by: {report.reporter.username}</p>
+              <p className="item-description">Reported user: {report.reported.username}</p>
+              <p className="item-description">Reason: {report.reason}</p>
+            </article>
+          ))}
         </div>
       </section>
-
     </main>
   );
 };
