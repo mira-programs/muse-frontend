fe04e4f236d324c3db69459fc2eab7ab41089d61
mms161 <144433947+mms161@users.noreply.github.com>
2024-04-24 07:44:36 +0300

    postitem.css done

--- muse-website/src/components/PostItem/PostItem.jsx
+++ muse-website/src/components/PostItem/PostItem.jsx
@@ -1,10 +1,11 @@
+import './PostItem.css'
 import { Link } from 'react-router-dom'
 import PostAuthor from '../PostAuthor/PostAuthor'
 
-export default function PostItem({Image, muserId, tag, title, des, postID}) {
+export default function PostItem({Image, muserId, tags, title, des, postID}) {
 
     const shorTitle = title.length > 40 ? title.substr(0, 30) + '...' : title;
-    const shorDes = des.length > 145 ? des.substr(0, 145) + '...' : des;
+    const shortDes = des.length > 145 ? des.substr(0, 145) + '...' : des;
 
   return (
     <article className="post">
@@ -15,10 +16,16 @@ export default function PostItem({Image, muserId, tag, title, des, postID}) {
           <Link to={`posts/${postID}`}>
             <h3>{shorTitle}</h3>
           </Link>
-          <p>{shorDes}</p>
+          <p>{shortDes}</p>
           <div className="post-footer">
             <PostAuthor muserID={muserId}/>
-            <Link to={`/posts/categories/${tag}`} className="btn btn-category">{tag}</Link>
+            {tags && tags.length > 0 && (
+                    <div className="post-tags">
+                        {tags.map((tag, index) => (
+                            <span key={index} className="tag">{typeof tag === 'string' ? tag : tag.name}</span>
+                        ))}
+                    </div>
+            )}
           </div>
         </div>
     </article>
