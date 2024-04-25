d89f8b7db2f5fc8a15f4822e30888bfbb44a858a
MayaTaliha <147243340+MayaTaliha@users.noreply.github.com>
2024-04-25 14:15:01 +0300

    message page (style may need improvement)

--- muse-website/src/pages/Profile/Profile.jsx
+++ muse-website/src/pages/Profile/Profile.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { FaEdit, FaCheckCircle } from "react-icons/fa";
 import { PiUserCircleDuotone } from "react-icons/pi";
 import './Profile.css';
